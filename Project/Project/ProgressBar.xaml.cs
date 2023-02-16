e16a8d7ebb01fa2946fa2a7c94a288e1d9148f42
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-02-16 23:58:22 +0300

    refactor: format code and rename labels' text

--- Project/Project/ProgressBar.xaml.cs
+++ Project/Project/ProgressBar.xaml.cs
@@ -13,7 +13,7 @@ public partial class ProgressBar : ContentPage
         source = new CancellationTokenSource();
         Button button = sender as Button;
         button.IsEnabled = false;
-        LoadLabel.Text = "Loading...";
+        LoadLabel.Text = "Computing...";
         ProgressResult.Text = "0";
         await Task.Run(() => ComputeIntegral(), source.Token);
     }
@@ -21,44 +21,37 @@ public partial class ProgressBar : ContentPage
     private void ComputeIntegral()
     {
         double step = 0.00001, integral = 0, progress = 0;
-        for (double x = 0; x <= 1; x += step)
-        {
-            if (source.IsCancellationRequested)
-            {
-                MainThread.BeginInvokeOnMainThread(() =>
-                {
+        for (double x = 0; x <= 1; x += step) {
+            if (source.IsCancellationRequested) {
+                MainThread.BeginInvokeOnMainThread(() => {
                     ProgressBarIndicator.Progress = 0;
                     ProgressResult.Text = "0%";
                     StartButton.IsEnabled = true;
-                    LoadLabel.Text = "Press start";
+                    LoadLabel.Text = "Task cancelled";
                 });
                 return;
             }
             integral += step * Math.Sin(x);
-            for (int i = 0; i < 10000; i++)
-            {
+            for (int i = 0; i < 10000; i++) {
                 int tmp = i * i;
             }
             progress = Math.Round(x, 2);
-            MainThread.BeginInvokeOnMainThread(() =>
-            {
+            MainThread.BeginInvokeOnMainThread(() => {
                 ProgressBarIndicator.Progress = progress;
                 ProgressResult.Text = Convert.ToInt32(progress * 100).ToString() + "%";
             });
         }
-        MainThread.BeginInvokeOnMainThread(() =>
-        {
+        MainThread.BeginInvokeOnMainThread(() => {
             ProgressBarIndicator.Progress = 1;
             ProgressResult.Text = "100%";
             StartButton.IsEnabled = true;
-            LoadLabel.Text = "Completed successfully!";
+            LoadLabel.Text = "Completed successfully!\nIntegral equals " + integral.ToString();
         });
     }
 
-    private void OnCancelClicked(object sender, EventArgs e)
+    private void OnCancelClicked(object sender, EventArgs e) 
     {
-        if (!source.IsCancellationRequested)
-        {
+        if (!source.IsCancellationRequested) {
             source.Cancel();
         }
     }
