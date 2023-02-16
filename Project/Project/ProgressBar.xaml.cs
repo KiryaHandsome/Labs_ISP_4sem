10b7feb574ebb7be902e80be193422427596f238
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-02-16 23:52:25 +0300

    feat: add 2 lab(progressBar)

---
+++ Project/Project/ProgressBar.xaml.cs
@@ -0,0 +1,65 @@
+namespace Project;
+
+public partial class ProgressBar : ContentPage
+{
+    private CancellationTokenSource source;
+    public ProgressBar()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnStartClicked(object sender, EventArgs e)
+    {
+        source = new CancellationTokenSource();
+        Button button = sender as Button;
+        button.IsEnabled = false;
+        LoadLabel.Text = "Loading...";
+        ProgressResult.Text = "0";
+        await Task.Run(() => ComputeIntegral(), source.Token);
+    }
+
+    private void ComputeIntegral()
+    {
+        double step = 0.00001, integral = 0, progress = 0;
+        for (double x = 0; x <= 1; x += step)
+        {
+            if (source.IsCancellationRequested)
+            {
+                MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    ProgressBarIndicator.Progress = 0;
+                    ProgressResult.Text = "0%";
+                    StartButton.IsEnabled = true;
+                    LoadLabel.Text = "Press start";
+                });
+                return;
+            }
+            integral += step * Math.Sin(x);
+            for (int i = 0; i < 10000; i++)
+            {
+                int tmp = i * i;
+            }
+            progress = Math.Round(x, 2);
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                ProgressBarIndicator.Progress = progress;
+                ProgressResult.Text = Convert.ToInt32(progress * 100).ToString() + "%";
+            });
+        }
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            ProgressBarIndicator.Progress = 1;
+            ProgressResult.Text = "100%";
+            StartButton.IsEnabled = true;
+            LoadLabel.Text = "Completed successfully!";
+        });
+    }
+
+    private void OnCancelClicked(object sender, EventArgs e)
+    {
+        if (!source.IsCancellationRequested)
+        {
+            source.Cancel();
+        }
+    }
+}
