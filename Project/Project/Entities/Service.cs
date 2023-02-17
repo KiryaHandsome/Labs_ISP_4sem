ec006dc89df753b3689731446dca53e048e29bdc
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-02-17 21:56:17 +0300

    fix: add annotation 'indexed'

--- Project/Project/Entities/Service.cs
+++ Project/Project/Entities/Service.cs
@@ -7,6 +7,7 @@ namespace Project.Entities
     {
         [PrimaryKey, AutoIncrement, Indexed]
         public int Id { get; set; }
+        [Indexed]
         public int RoomId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
